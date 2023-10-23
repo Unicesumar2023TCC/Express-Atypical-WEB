bc37ae9d4f226ecf900733c836d3421c290c03e0
Matheus Henrique de Oliveira <matheus.253.oliveira@gmail.com>
2023-10-23 12:27:05 -0300

    Update api.tsx

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -3,9 +3,11 @@ import Axios from 'axios';
 const api = Axios.create({
     baseURL: 'https://expressatypical.inffus.com.br/',
     headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
     },
     withCredentials: true
 });
+
 
 export default api;
